24418a26b0499cebbb6a4c35d518c6419e5f4414
Ling <lzhou27@jaguarlandrover.com>
2024-09-18 17:04:09 +0800

    update

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,22 +1,14 @@
 import pinyin from 'tiny-pinyin';
 import type { AnyObject, CamelCaseType } from './interface';
+import _camelCase from 'lodash.camelcase';
+import _upperFirst from 'lodash.upperfirst';
 
 export function camelCase(str: string, type: CamelCaseType = 1) {
-  const words = str.match(/[a-zA-Z0-9]+/g);
-  if (!words) {
-    return '';
+  const string = _camelCase(str)
+  if(type == 2){
+    return _upperFirst(string)
   }
-  return words
-    .map((word, index) => {
-      if (index === 0) {
-        return word.replace(
-          word[0],
-          type === 2 ? word[0].toUpperCase() : word[0].toLowerCase(),
-        );
-      }
-      return word.charAt(0).toUpperCase() + word.slice(1);
-    })
-    .join('');
+  return string
 }
 
 export function getPathName(path: string, maxLength = 5) {
